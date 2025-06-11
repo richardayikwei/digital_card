111cf6fc9d15483935de9078641bfe3e41d5b20d
Richard Ayikwei <richardayikweibee@gmail.com>
2025-06-11 12:14:00 +0000

    ref(card): redoing code

--- card.py
+++ card.py
@@ -1,5 +1,29 @@
 import qrcode
+from PIL import Image
 
-url = "https://raw.githubusercontent.com/YOUR_USERNAME/digital-card/main/richard_ayikwei.vcf"  # or Google Drive link
-img = qrcode.make(url)
-img.save("richard_vcard_qr.png")
+vcard_data = """
+BEGIN:VCARD
+VERSION:3.0
+N:Ayikwei;Richard
+FN:Richard Ayikwei
+ORG:Kofa Technologies Limited
+URL:https://www.linkedin.com/in/richard-ayikwei-48b171112
+EMAIL:richard.ayikwei@kofa.co
+TEL:+233509805280
+TITLE:Network Management Associate
+END:VCARD
+"""
+
+qr = qrcode.QRCode(
+    version=None,  # auto
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # high error correction
+    box_size=10,
+    border=4
+)
+
+qr.add_data(vcard_data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img = img.resize((300, 300), Image.LANCZOS)
+img.save("richard_ayikwei_vcard.png")
